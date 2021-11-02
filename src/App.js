ab19b85e28ed3be34a0e509a67ead7fd9fb9b77d
hyuby <hyuby@plateer.com>
2021-11-02 08:11:07 +0000

    Add new blog version

--- src/App.js
+++ src/App.js
@@ -64,7 +64,7 @@ function App(props) {
             align="center"
             className={classes.title}
           >
-            EKS DEMO Blog
+            EKS DEMO Blog Hyuby
           </Typography>
           {new Date().toLocaleTimeString()}
         </Toolbar>
